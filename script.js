ca20739101be9c5bc0bb2d7a5d998ba98975e5b9
hassan <hassanpudineh67@gmail.com>
2022-12-02 12:48:12 +0330

    fix: add hold function

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const diceEl = document.querySelector('.dice');
 const  btnRoll = document.querySelector('.btn--roll');
 const  btnNew = document.querySelector('.btn--new');
 const  btnHold = document.querySelector('.btn--hold');
+const section0 = document.querySelector('.player--0');
+const section1 = document.querySelector('.player--1');
 
 score0El.textContent = score1El.textContent = 0;
 diceEl.classList.add('hidden');
@@ -15,15 +17,40 @@ let generateRandomNumber = function() {
       return Math.trunc((Math.random() * 6)) + 1
 }
 let currentScore = 0;
+let activePlayer = 0;
+
+document.querySelector(`.player--${activePlayer}`).classList.add('player--active')
+
+let player0Score = null;
+let player1Score = null;
+
+function changePlayer(){
+      player0Score = player1Score = 0;
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      activePlayer = activePlayer === 0 ? 1: 0;
+      document.querySelectorAll('.player').forEach(el => {
+            el.classList.contains('player--active') ? el.classList.remove('player--active') : el.classList.add('player--active')
+      })
+}
+
 btnRoll.addEventListener('click', function(){
       const randomNumber = generateRandomNumber();
       diceEl.src = `dice-${randomNumber}.png`;
       diceEl.classList.remove('hidden');
       if(randomNumber !== 1){
-            currentScore += randomNumber;
-            current0El.textContent = currentScore;
+            activePlayer ? player1Score += randomNumber : player0Score += randomNumber;
+            document.getElementById(`current--${activePlayer}`).textContent = activePlayer ? player1Score : player0Score;
+
+            if((activePlayer ? player1Score : player0Score) >= 20) {
+                  document.getElementById(`current--${activePlayer}`).classList.add('player--winner')
+                  document.getElementById(`current--${activePlayer}`).classList.remove('player--active')
+            }
       } else {
-            currentScore += randomNumber;
-            current1El.textContent = currentScore;
+            changePlayer();
       }
+})
+
+btnHold.addEventListener('click', function(){
+      activePlayer ? score1El.textContent = Number(score1El.textContent) + player1Score : score0El.textContent = Number(score0El.textContent) + player0Score;
+      changePlayer();
 })
